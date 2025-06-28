5cf00f5717b027199762aaa6f0234e1619060315
shiva19074 <shivaaggarwal976@gmail.com>
2025-06-28 20:29:54 +0530

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_KEY = "pk-VrdssVWiTTwhuwubSOCldRGLLnECuwHBnZXzAGkDgedkPNTD"
+API_KEY = "pk-HzqqRdpeGoCljVOEEPzEAcqHdHUcAaogRHSZWMyyPhkPlkEi"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 chat_memory = {}
